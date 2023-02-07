29ef94ca9d472f7e10598aa67b39f37308e31c8e
MitulBhavsarG <58432548+MitulBhavsarG@users.noreply.github.com>
2023-02-07 11:16:43 +0530

    Dev/mitul (#2)
    * Initial setup
    
    * Update README.md
    
    * Logger added and some problem solved
    
    * Added resolved problems

---
+++ src/neetcode/mitul/ArrayHashing/topKFrequent.ts
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/top-k-frequent-elements/
+
+export function topKFrequent(nums: number[], k: number): number[] {
+  const map: { [key: string]: number } = {};
+  for (let num of nums) {
+    if (map[num] === undefined) map[num] = 1;
+    else map[num]++;
+  }
+
+  return (Object.keys(map) as any)
+    .sort((a: string, b: string) => (map[b] - map[a]))
+    .slice(0, k)
+};
--- src/neetcode/mitul/index.ts
+++ src/neetcode/mitul/index.ts
@@ -1,6 +1,9 @@
 import { logger } from '../../../util/logger/logger'
 import { containsDuplicate } from './ArrayHashing/containsDuplicate'
 import { groupAnagrams } from './ArrayHashing/groupAnagrams'
+import { isValidSudoku } from './ArrayHashing/isValidSudoku'
+import { productExceptSelf } from './ArrayHashing/productExceptSelf'
+import { topKFrequent } from './ArrayHashing/topKFrequent'
 import { twoSum } from './ArrayHashing/twoSum'
 
 function containsDuplicateTests() {
@@ -20,8 +23,51 @@ function groupAnagramsTests() {
   logger.info(groupAnagrams(['a'])) // [["a"]]
 }
 
+function topKFrequentTests() {
+  logger.info(topKFrequent([1, 1, 1, 1, 2, 2, 3], 2)) // [1,2]
+  logger.info(topKFrequent([1], 1)) // [1]
+}
+
+function productExceptSelfTests() {
+  logger.info(productExceptSelf([1, 2, 3, 4])) // [24,12,8,6]
+  logger.info(productExceptSelf([-1, 1, 0, -3, 3])) // [0,0,9,0,0]
+}
+
+function isValidSudokuTests() {
+  logger.info(
+    isValidSudoku([
+      ['5', '3', '.', '.', '7', '.', '.', '.', '.'],
+      ['6', '.', '.', '1', '9', '5', '.', '.', '.'],
+      ['.', '9', '8', '.', '.', '.', '.', '6', '.'],
+      ['8', '.', '.', '.', '6', '.', '.', '.', '3'],
+      ['4', '.', '.', '8', '.', '3', '.', '.', '1'],
+      ['7', '.', '.', '.', '2', '.', '.', '.', '6'],
+      ['.', '6', '.', '.', '.', '.', '2', '8', '.'],
+      ['.', '.', '.', '4', '1', '9', '.', '.', '5'],
+      ['.', '.', '.', '.', '8', '.', '.', '7', '9'],
+    ]),
+  ) // true
+
+  logger.info(
+    isValidSudoku([
+      ['8', '3', '.', '.', '7', '.', '.', '.', '.'],
+      ['6', '.', '.', '1', '9', '5', '.', '.', '.'],
+      ['.', '9', '8', '.', '.', '.', '.', '6', '.'],
+      ['8', '.', '.', '.', '6', '.', '.', '.', '3'],
+      ['4', '.', '.', '8', '.', '3', '.', '.', '1'],
+      ['7', '.', '.', '.', '2', '.', '.', '.', '6'],
+      ['.', '6', '.', '.', '.', '.', '2', '8', '.'],
+      ['.', '.', '.', '4', '1', '9', '.', '.', '5'],
+      ['.', '.', '.', '.', '8', '.', '.', '7', '9'],
+    ]),
+  ) // false
+}
+
 export function mitulTests() {
   containsDuplicateTests()
   twoSumTests()
   groupAnagramsTests()
+  topKFrequentTests()
+  productExceptSelfTests()
+  isValidSudokuTests()
 }
