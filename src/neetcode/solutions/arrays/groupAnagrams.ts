fa8710fb779be34b9a66bff7be211e16660c8828
MitulBhavsarG <bhavsarmitul.005@gmail.com>
2023-05-10 08:15:36 +0530

    Husky test commit

--- src/neetcode/solutions/arrays/groupAnagrams.ts
+++ src/neetcode/solutions/arrays/groupAnagrams.ts
@@ -1,12 +1,12 @@
 // https://leetcode.com/problems/group-anagrams/
 
 export function groupAnagrams(strs: string[]) {
-  const map:{ [key: string]: string[] } = {}
+  const map: { [key: string]: string[] } = {}
   strs.forEach((str) => {
-    const sortedStr = str.split("").sort().join("");
+    const sortedStr = str.split('').sort().join('')
     if (!map[sortedStr]) map[sortedStr] = [str]
     else map[sortedStr].push(str)
-  });
+  })
 
   return Object.values(map)
 }
