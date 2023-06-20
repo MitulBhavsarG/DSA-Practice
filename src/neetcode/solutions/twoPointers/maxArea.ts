ba46584b393d05b7544f9b7b780357711abd33b3
MitulBhavsarG <bhavsarmitul.005@gmail.com>
2023-06-20 15:45:54 +0530

    Max area

---
+++ src/neetcode/solutions/twoPointers/maxArea.ts
@@ -0,0 +1,25 @@
+// brut force
+export function brutForceMaxArea(height: number[]): number {
+  let maxArea = 0
+  for (let i = 0; i < height.length - 1; i++) {
+    for (let j = i + 1; j < height.length; j++) {
+      const currentHeight = Math.min(height[i], height[j])
+      const currentArea = (j - i) * currentHeight
+      maxArea = Math.max(currentArea, maxArea)
+    }
+  }
+  return maxArea
+}
+
+export function maxArea(height: number[]): number {
+  let maxArea = 0
+  let left = 0,
+    right = height.length - 1
+  while (left < right) {
+    const area = (right - left) * Math.min(height[left], height[right])
+    maxArea = Math.max(maxArea, area)
+    if (height[left] < height[right]) left++
+    else right--
+  }
+  return maxArea
+}
--- src/neetcode/tests/twoPointers.ts
+++ src/neetcode/tests/twoPointers.ts
@@ -3,15 +3,16 @@ import { logger } from '../../../util/logger'
 import { isPalindrome } from '../solutions/twoPointers/validPalindrome'
 import { twoSum } from '../solutions/twoPointers/twoSum'
 import { threeSum } from '../solutions/twoPointers/threeSum'
+import { maxArea } from '../solutions/twoPointers/maxArea'
 
 export const twoSumTests = () => {
-  // logger.info(twoSum([2, 7, 11, 15], 9))
-  // logger.info(twoSum([2, 3, 4], 6))
-  // logger.info(twoSum([0, 0, 3, 4], 0))
-  // logger.info(twoSum([5, 25, 75], 100))
+  logger.info(twoSum([2, 7, 11, 15], 9))
+  logger.info(twoSum([2, 3, 4], 6))
+  logger.info(twoSum([0, 0, 3, 4], 0))
+  logger.info(twoSum([5, 25, 75], 100))
   logger.info(twoSum([3, 24, 50, 79, 88, 150, 345], 200))
-  // logger.info(twoSum([-1, 0], -1))
-  // logger.info(twoSum([1, 2, 2, 2, 2, 2, 2, 2, 3, 4, 5], 5))
+  logger.info(twoSum([-1, 0], -1))
+  logger.info(twoSum([1, 2, 2, 2, 2, 2, 2, 2, 3, 4, 5], 5))
 }
 
 export const threeSumTests = () => {
@@ -20,7 +21,13 @@ export const threeSumTests = () => {
   logger.info(JSON.stringify(threeSum([0, 0, 0])))
 }
 
+export const maxAreaTests = () => {
+  logger.info(JSON.stringify(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7])))
+  logger.info(JSON.stringify(maxArea([1, 1])))
+}
+
 export const twoPointersTests = () => {
   // twoSumTests()
-  threeSumTests()
+  // threeSumTests()
+  maxAreaTests()
 }
